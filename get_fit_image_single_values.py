34ac17fc62db3d0638e846616980688a0b735370
mrosanes <mrosanes@cells.es>
2021-12-27 16:30:25 +0100

    Add and update function verify_pixel
    Function verify_pixel is a helper function with debug
    purposes, which compares an object nomalized file pixel value in
    a given position, with the freshly calculated pixel value in the
    same position, calculated taking into account the master_bias
    pixel value, the FF pixel value (in the given filter), the object
    pixel values, and the exposure times (as an option).

--- get_fit_image_single_values.py
+++ get_fit_image_single_values.py
@@ -1,6 +1,7 @@
 """Get and print specific values and data from images for 
    debugging purposes"""
 
+import os
 import numpy as np
 from astropy.io import fits
 
@@ -31,91 +32,118 @@ def get_fit_img_shape_and_dtype(img_name):
     print("Image type: " + str(img.dtype))
 
 
-def verify_pixel():
-    # Compare automatic and manual and assert they are almost equal
-    pass
+def verify_pixel(image_number=1, filter="I", pixel_pos=[0, 0], 
+                 master_bias_fname="master_bias.fit" , 
+                 master_ff_fname="master_ff_I.fit", 
+                 exposure_times_used=True):
+    """
+    Compare object nomalized file pixel value in a given position, 
+    with the freshly calculated pixel value in the same position, taking 
+    into account the master_bias pixel value, the FF pixel value (in 
+    the given filter), the object pixel values, and the exposure 
+    times (as an option).
+    Inputs:
+      - image_number: number of image where the pixel to verify is located
+      - filter: filter in which the image was acquired: "I" or "B"
+      - pixel_pos: list of two integers indicating the pixel position
+      - master_bias_fname: Master Bias file name
+      - master_ff_fname: Master FlatField file name in the given filter
+      - exposure_times_used: indicate if exposure times were used or not
+                             in the computation of the normalized image
+    """
 
+    ## Important variables ##
+ 
+    # Image number with four digits:
+    img_num_str = "{:04}".format(image_number)
     
+    # Pixel [x, y]:
+    x = pixel_pos[0]
+    y = pixel_pos[1]
+
+    # Object image names:
+    obj_img_fname = "WASP-33-" + img_num_str + filter + ".fit"
+    obj_img_norm_fname = "WASP-33-" + img_num_str + filter + "_norm.fit"
+
+    print("Image in " + filter)
+                
+    # Exposure times in case they were used to compute the normalized images
+    if exposure_times_used:
+        if filter == "I":
+            exp_time_obj = 15.0
+            exp_time_ff = 6.0
+        elif filter == "B":
+            exp_time_obj = 30.0
+            exp_time_ff = 6.0 
+        else:
+            raise("Choose a valid option for the filter: I or B")      
+    ##########################################################################
+    
+    # master_bias value for pixel [x, y]
+    vals_in_xy_bias = get_values_and_dtype(
+        master_bias_fname, values_positions=[pixel_pos])
+    val_in_xy_bias = vals_in_xy_bias[0] 
+    
+    ##########################################################################
+    ####### Value on pixel [x, y] on computed obj_img_norm_fname #############    
+
+    print(obj_img_norm_fname + " pixel " + str(pixel_pos) + " value:")
+    vals_in_xy_obg_img_norm = get_values_and_dtype(
+        obj_img_norm_fname, values_positions=[pixel_pos])
+    val_in_xy_obg_img_norm = vals_in_xy_obg_img_norm[0]                                               
+    print(val_in_xy_obg_img_norm)
+
+    ##########################################################################
+    ### WASP-33-0073I Pixel[x, y] normalized value, 'manually' calculated ####
+    
+    # FF Image Pixel Value
+    vals_in_xy_ff = get_values_and_dtype(master_ff_fname, 
+                                         values_positions=[pixel_pos])
+    val_in_xy_ff = vals_in_xy_ff[0]
+    
+    # Raw Object Image Pixel Value (before normalization)                                
+    vals_in_xy_obj_img = get_values_and_dtype(
+        obj_img_fname, values_positions=[pixel_pos])
+    val_in_xy_obj_img = vals_in_xy_obj_img[0]
+
+    numerator = exp_time_ff * (val_in_xy_obj_img - val_in_xy_bias)
+    denominator = exp_time_obj * val_in_xy_ff
+    # Freshly calculated value for the pixel in the specified position
+    just_computed_xy_pix_val_norm = np.float32(numerator / denominator)
+    obj_name = os.path.splitext(obj_img_fname)[0]
+    print(obj_name + " pixel " + str(pixel_pos) + " normalized value,"
+          + " freshly calculated:")
+    print(just_computed_xy_pix_val_norm)
+    
+    # Assert that both values (the one coming from the normalized fit image,
+    #   and the freshly computed value), are almost equal, to 4 decimal places
+    try:                          
+        np.testing.assert_almost_equal(val_in_xy_obg_img_norm, 
+                                       just_computed_xy_pix_val_norm, 4)
+    except Exception(e):
+        print(e)
+    print("\nThe test has passed:")
+    print("Pixel value in normalized image and pixel value freshly" 
+          + " calculated are equal")
+
+
 if __name__ == "__main__":
 
+    print()
+    # Image Dimensions and DataType
     get_fit_img_shape_and_dtype("master_bias.fit")
-    print("\n")
-            
-    # Pixel [x, y]
-    x = 531
-    y = 2047
-    pixel = [x, y]
-
-    # master_bias Pixel[x, y]
-    print("Pixel " + str(pixel) + " from master_bias.fit:")
-    vals_in_xy_bias = get_values_and_dtype("master_bias.fit", 
-                                           values_positions=[[x, y]])
-    val_in_xy_bias = vals_in_xy_bias[0] 
-    print(val_in_xy_bias)
+    print()
+      
+    # Verification of the value of single pixel between normalized image
+    #     and freshly calculated pixel value, taking into account the 
+    #     master_bias pixel value, the FF pixel value (in the given filter),
+    #     the object pixel values, and the exposure times (as an option).  
+    verify_pixel(image_number=53, filter="I", pixel_pos=[253, 2042], 
+                 master_ff_fname="master_ff_I.fit")
+    print("\n")             
+    verify_pixel(image_number=53, filter="B", pixel_pos=[1056, 35], 
+                 master_ff_fname="master_ff_B.fit")
+    print()    
     
-    ##########################################################################
-    ######### Pixel[x, y] on computed WASP-33-0073I_norm.fit #################
-    print("\nI")
-    exp_time_obj_I = 15.0
-    exp_time_ff_I = 6.0
-
-    print("Pixel " + str(pixel) + " on computed WASP-33-0073II_norm.fit:")
-    vals_in_xy_obg_img_norm = get_values_and_dtype("WASP-33-0073I_norm.fit", 
-                                                   values_positions=[[x, y]])
-    val_in_xy_obg_img_norm = vals_in_xy_obg_img_norm[0]                                               
-    print(val_in_xy_obg_img_norm)
-
-    ### WASP-33-0073I Pixel[x, y] normalized value, 'manually' calculated ####
-    print("Pixel " + str(pixel) + " from master_ff_I.fit:")
-    vals_in_xy_ff_I = get_values_and_dtype("master_ff_I.fit", 
-                                           values_positions=[[x, y]])
-    val_in_xy_ff_I = vals_in_xy_ff_I[0]
-    print(val_in_xy_ff_I)
-                                          
-    print("Pixel " + str(pixel) + " from WASP-33-0073I.fit:")                                      
-    vals_in_xy_obj_img = get_values_and_dtype("WASP-33-0073I.fit", 
-                                              values_positions=[[x, y]])
-    val_in_xy_obj_img = vals_in_xy_obj_img[0]
-    print(val_in_xy_obj_img)
-    
-    numerator = exp_time_ff_I * (val_in_xy_obj_img - val_in_xy_bias)
-    denominator = exp_time_obj_I * val_in_xy_ff_I
-    manually_calculated_xy_pixel_norm = np.float32(numerator / denominator)
-    print("WASP-0073I pixel " + str(pixel) + " normalized value,"
-          + " 'manually' calculated:")
-    print(manually_calculated_xy_pixel_norm)
-    
-    ##########################################################################
-    ######### Pixel[x, y] on computed WASP-33-0253B_norm.fit #################
-    print("\nB")
-    exp_time_obj_B = 30.0
-    exp_time_ff_B = 6.0    
-
-    print("Pixel " + str(pixel) + " on computed WASP-33-0253B_norm.fit:")
-    vals_in_xy_obg_img_norm = get_values_and_dtype("WASP-33-0253B_norm.fit", 
-                                                   values_positions=[[x, y]])
-    val_in_xy_obg_img_norm = vals_in_xy_obg_img_norm[0]                                               
-    print(val_in_xy_obg_img_norm)
-
-    ### WASP-33-0253B Pixel[x, y] normalized value, 'manually' calculated ####
-    print("Pixel " + str(pixel) + " from master_ff_B.fit:")
-    vals_in_xy_ff_B = get_values_and_dtype("master_ff_B.fit", 
-                                           values_positions=[[x, y]])
-    val_in_xy_ff_B = vals_in_xy_ff_B[0]
-    print(val_in_xy_ff_B)
-                                          
-    print("Pixel " + str(pixel) + " from WASP-33-0253B.fit:")                                      
-    vals_in_xy_obj_img = get_values_and_dtype("WASP-33-0253B.fit", 
-                                              values_positions=[[x, y]])
-    val_in_xy_obj_img = vals_in_xy_obj_img[0]
-    print(val_in_xy_obj_img)
-    
-    numerator = exp_time_ff_B * (val_in_xy_obj_img - val_in_xy_bias)
-    denominator = exp_time_obj_B * val_in_xy_ff_B
-    manually_calculated_xy_pixel_norm = np.float32(numerator / denominator)
-    print("WASP-0253B pixel " + str(pixel) + " normalized value,"
-          + " 'manually' calculated:")
-    print(manually_calculated_xy_pixel_norm)
-    ##########################################################################
 
 
